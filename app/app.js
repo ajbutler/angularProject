75ecd4554f09f68ad1f76a6a5ffb6d8cd351dd34
Adam Butler <abutler-@BigMac.local>
2015-11-10 22:58:36 +0000

    Additional data / styles

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'phonecatAnimations',
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
